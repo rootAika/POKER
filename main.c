31d830822e5b5df940a6845753ed5ed5f999ad27
Mathis JOSIEN <mathis.josien@free.fr>
2020-04-15 10:09:09 +0200

    generate figure

--- main.c
+++ main.c
@@ -1,10 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
 
-void functionone();
 
-void functiontwo();
+/********************************* valeurs***************************************/
+char values []={'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'};
+char figures[]={'C', 'D', 'H', 'S'}; //C=Trèfle H=Coeur D=Carreau S=Pique
+
+
+/*********************************structure*********************************/
+typedef struct carte {
+    char valeur;
+    char figure;
+}carte;
+
+typedef struct main
+{
+    carte cartes[5];
+}mainjoueur;
+
+/*******************************prototypes**********************************/
+mainjoueur generatehand();
+void affichermain(mainjoueur);
+char generatefigure();
+
+/********************************choix figures et valeurs*******************/
+
+
+
+
+
+char generatevalue();
+
+char generatefigure() {
+    srand(time(NULL)); //initialisation du nb aléatoire
+    int nb2 = rand() %4;
+    printf("%c", figures[nb2]);
+    return figures[nb2];
+}
+
+/******************************************Génération carte********************************/
+
+mainjoueur generatehand() {
+    carte main;
+    mainjoueur result;
+    return result;
+}
+
 
 int main() {
-    printf("Version 3\n");
+    generatefigure();
     return 0;
 }
